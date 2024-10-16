ba55e6419f573b5459e7e509b4796979c1ee67e6
user <user@usernoMacBook-Air.local>
2024-10-16 17:56:40 +0900

    register 17ch ok🍷

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
+
+from userauths.models import Profile
+
+
+
+
+
+def RegisterView(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        messages.warning(request, f"Hey {request.user.username}, you are already logged in")
+        return redirect('core:feed')   
+
+    form = UserRegisterForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        full_name = form.cleaned_data.get('full_name')
+        phone = form.cleaned_data.get('phone')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+
+        user = authenticate(email=email, password=password)
+        login(request, user)
+
+        messages.success(request, f"Hi {request.user.username}, your account have been created successfully.")
+
+        profile = Profile.objects.get(user=request.user)
+        profile.full_name = full_name
+        profile.phone = phone
+        profile.save()
+
+        return redirect('core:feed')
+    
+    context = {
+        'form':form
+    }
+    
+    return render(request, 'userauths/sign-up.html', context)
