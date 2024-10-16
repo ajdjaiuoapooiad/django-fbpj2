2502501602e561d74b78c4345a7dfde336c3fb35
user <user@usernoMacBook-Air.local>
2024-10-16 14:45:58 +0900

    11ch profile 🍷

--- userauths/models.py
+++ userauths/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
-
+from shortuuid.django_fields import ShortUUIDField
 
 
 
@@ -13,12 +13,25 @@ GENDER = (
     ("male", "Male"),
 )
 
+RELATIONSHIP = (
+    ("single","Single"),
+    ("married","married"),
+    ("inlove","In Love"),
+)
+
+WHO_CAN_SEE_MY_FRIENDS = (
+    ("Only Me","Only Me"),
+    ("Everyone","Everyone"),
+)
+
 
 
 def user_directory_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (instance.user.id, ext)
     return 'user_{0}/{1}'.format(instance.user.id,  filename)
+
+
 
 class User(AbstractUser):
     full_name = models.CharField(max_length=1000, null=True, blank=True)
@@ -34,3 +47,37 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.username)
+    
+class Profile(models.Model):
+    pid = ShortUUIDField(length=7, max_length=25, alphabet="abcdefghijklmnopqrstuvxyz123")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cover_image = models.ImageField(upload_to=user_directory_path, default="cover.jpg", blank=True, null=True)
+    image = models.ImageField(upload_to=user_directory_path, default="default.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=1000, null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    about_me = models.CharField( max_length=1000,null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.CharField(max_length=100, choices=GENDER, null=True, blank=True)
+    relationship = models.CharField(max_length=100, choices=RELATIONSHIP, null=True, blank=True, default="single")
+    friends_visibility = models.CharField(max_length=100, choices=WHO_CAN_SEE_MY_FRIENDS, null=True, blank=True, default="Everyone")
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
+    working_at = models.CharField(max_length=1000, null=True, blank=True)
+    instagram = models.URLField(default="https://instagram.com/", null=True, blank=True)
+    whatsApp = models.CharField(default="+123 (456) 789", max_length=100, blank=True, null=True)
+    verified = models.BooleanField(default=False)
+    followers = models.ManyToManyField(User, blank=True, related_name="followers")
+    followings = models.ManyToManyField(User, blank=True, related_name="followings")
+    friends = models.ManyToManyField(User, blank=True, related_name="friends")
+    
+    blocked = models.ManyToManyField(User, blank=True, related_name="blocked")
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+
+
+    def __str__(self):
+        return self.user.username
+
+
