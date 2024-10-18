675d65cfb7c148b184f77c2bb5ef0e6647f2a53a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-18 10:59:07 +0900

    27ch

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
 from core.models import VISIBILITY, Post
+import shortuuid
+from django.utils.text import slugify
+from django.utils.timesince import timesince
+
+
+
+
+
 
 
 def index(request):
@@ -11,3 +20,37 @@ def index(request):
         "posts":posts
     }
     return render(request, "core/index.html", context)
+
+
+def create_post(request):
+
+    if request.method == 'POST':
+        title = request.POST.get('post-caption')
+        visibility = request.POST.get('visibility')
+        image = request.FILES.get('post-thumbnail')
+
+        print("Title ============", title)
+        print("thumbnail ============", image)
+        print("visibility ============", visibility)
+
+        uuid_key = shortuuid.uuid()
+        uniqueid = uuid_key[:4]
+
+        if title and image:
+            post = Post(title=title, image=image, visibility=visibility, user=request.user, slug=slugify(title) + "-" + str(uniqueid.lower()))
+            post.save()
+
+            
+            return JsonResponse({'post': {
+                'title': post.title,
+                'image_url': post.image.url,
+                "full_name":post.user.profile.full_name,
+                "profile_image":post.user.profile.image.url,
+                "date":timesince(post.date),
+                "id":post.id,
+            }})
+        else:
+            return JsonResponse({'error': 'Invalid post data'})
+
+    return JsonResponse({"data":"Sent"})
+
