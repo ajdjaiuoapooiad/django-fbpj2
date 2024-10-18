6cf94082c116c8c0b6586b5e2ea5e18e7b092c2b
user <user@usernoMacBook-Air.local>
2024-10-18 10:34:45 +0900

    26ch ok index🍷

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from core.models import VISIBILITY, Post
 
 
 def index(request):
-    return render(request,'core/index.html')
+    posts = Post.objects.filter(active=True, visibility="Everyone")
+
+    context = {
+        "posts":posts
+    }
+    return render(request, "core/index.html", context)
