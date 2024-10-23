8fbf25b6c4c0f3c400603f918955c0a64d9ea8a8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-23 11:31:52 +0900

    chat-detail ok

--- core/views.py
+++ core/views.py
@@ -331,7 +331,52 @@ def inbox(request):
     }
     return render(request, 'chat/inbox.html', context)
 
-  
+@login_required
+def inbox_detail(request, username):
+    user_id = request.user
+    message_list = ChatMessage.objects.filter(
+        id__in =  Subquery(
+            User.objects.filter(
+                Q(sender__reciever=user_id) |
+                Q(reciever__sender=user_id)
+            ).distinct().annotate(
+                last_msg=Subquery(
+                    ChatMessage.objects.filter(
+                        Q(sender=OuterRef('id'),reciever=user_id) |
+                        Q(reciever=OuterRef('id'),sender=user_id)
+                    ).order_by('-id')[:1].values_list('id',flat=True) 
+                )
+            ).values_list('last_msg', flat=True).order_by("-id")
+        )
+    ).order_by("-id")
+
+
+    
+    sender = request.user
+    receiver = User.objects.get(username=username)
+    receiver_details = User.objects.get(username=username)
+    
+    messages_detail = ChatMessage.objects.filter(
+        Q(sender=sender, reciever=receiver) | Q(sender=receiver, reciever=sender)
+    ).order_by("date")
+
+    messages_detail.update(is_read=True)
+    
+    if messages_detail:
+        r = messages_detail.first()
+        reciever = User.objects.get(username=r.reciever)
+    else:
+        reciever = User.objects.get(username=username)
+
+    context = {
+        'message_detail': messages_detail,
+        "reciever":reciever,
+        "sender":sender,
+        "receiver_details":receiver_details,
+        "message_list":message_list,
+    }
+    return render(request, 'chat/inbox_detail.html', context)
+
 
 
 
